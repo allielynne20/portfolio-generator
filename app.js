4b10eb5dca8f7ff58acde33f606bc5e5cbec44c3
allielynne20 <allielynne20@gmail.com>
2020-07-30 22:12:09 -0400

    quit and finished the project

--- app.js
+++ app.js
@@ -157,7 +157,24 @@ promptUser()
         });
     });
 
-
+// promptUser()
+//     .then(promptProject)
+//     .then(portfolioData => {
+//         return generatePage(portfolioData);
+//     })
+//     .then(pageHTML => {
+//         return writeFile(pageHTML);
+//     })
+//     .then(writeFileResponse => {
+//         console.log(writeFileResponse);
+//         return copyFile();
+//     })
+//     .then(copyFileResponse => {
+//         console.log(copyFileResponse);
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     });
 
 // console.log('Hello Node');node app.js
 //example of displaying error bc doc isn't defined
